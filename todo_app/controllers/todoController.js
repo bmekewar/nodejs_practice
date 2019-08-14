538e74da55d12665517dfbb8330f515782153622
Bala M <b_mekewar@yahoo.com>
2019-08-15 00:20:14 +0530

    todo updated

--- todo_app/controllers/todoController.js
+++ todo_app/controllers/todoController.js
@@ -1,22 +1,23 @@
 var bodyParser = require('body-parser');
 var data = [{ item: 'tea' }, { item: 'coffee' }, { item: 'milk' }];
-var urlEncodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app) {
 
-    app.get('/', function(req, res) {
+    /*app.get('/', function(req, res) {
         res.render('todo', { todos: data });
-    });
+    });*/
     app.get('/todo', function(req, res) {
         res.render('todo', { todos: data });
     });
-    app.post('/todo', urlEncodedParser, function(req, res) {
+    app.post('/todo', urlencodedParser, function(req, res) {
         data.push(res.body);
         res.json(data);
     });
-    app.delete('/todo', function(req, res) {
+    app.delete('/todo/:item', function(req, res) {
         data = data.filter(function(todo) {
             return todo.item.replace(/ /g, '-') !== req.params.item;
         });
+        res.json(data);
     });
 };
