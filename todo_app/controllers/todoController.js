2ee0a100b28e2daf59785d68aa079cd2714a1007
Bala M <b_mekewar@yahoo.com>
2019-08-14 23:53:40 +0530

    updated todo app changes

--- todo_app/controllers/todoController.js
+++ todo_app/controllers/todoController.js
@@ -1,21 +1,22 @@
-var data = [{item: 'tea'}, {item:'coffee'},{item:'milk'}];
 var bodyParser = require('body-parser');
+var data = [{ item: 'tea' }, { item: 'coffee' }, { item: 'milk' }];
+var urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
-var urlEncodedParser = bodyParser.urlencoded({extended:false});
+module.exports = function(app) {
 
-module.exports = function(app){
-
-    app.get('/', function(req,res){
-        res.render('todo',{todo: data});
+    app.get('/', function(req, res) {
+        res.render('todo', { todos: data });
     });
-    app.get('/todo', function(req,res){
-        res.render('todo');
+    app.get('/todo', function(req, res) {
+        res.render('todo', { todos: data });
     });
-    app.post('/todo',urlEncodedParser,function(req,res){
-        console.log(req.body);
-        data.push(req.body);
-        res.push(data);
+    app.post('/todo', urlEncodedParser, function(req, res) {
+        data.push(res.body);
+        res.json(data);
     });
-    app.delete('/todo', function(req,res){
+    app.delete('/todo', function(req, res) {
+        data = data.filter(function(todo) {
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
     });
 };
